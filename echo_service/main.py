af6376dcd463ed56aceff8762a5fd33b8fb434ea
jamesduan <jamesduanling@hotmail.com>
2015-10-23 17:00:39 +0800

    pyrestfull main.py

--- echo_service/main.py
+++ echo_service/main.py
@@ -1,21 +1,15 @@
 #!/usr/bin/env python
 
-#import json
-#import uuid
+import os
+import logging
 
-#from bs4 import BeautifulSoup
-##from tornado.web import Application, RequestHandler
-#from tornado.httpserver import HTTPServer
-#from tornado.options import define, options, parse_command_line
-#from tornado.ioloop import IOLoop
-#
-#define("port", default=8888, type=int)
-#
-import tornado.ioloop
+from tornado.options import define, options, parse_command_line
+from tornado.ioloop import IOLoop
 import pyrestful.rest
-
 from pyrestful import mediatypes
-from pyrestful.rest import get
+from pyrestful.rest import get, post
+
+define("port", default=8888, type=int)
 
 class EchoService(pyrestful.rest.RestHandler):
 
@@ -23,55 +17,37 @@ class EchoService(pyrestful.rest.RestHandler):
 	def sayHello(self, name):
 		return {"hello" : name}
 
-#class UploadHtmlHandler(RequestHandler):
-#
-#    def post(self):
-#        soup = BeautifulSoup(self.request.body)
-#        print 'title: ', str(soup.find_all('title'))[0]
-#        print 'body: ', soup.find_all('body')
-#        self.write('200')
-#
-#class LoginHandler(RequestHandler):
-#
-#	def post(self):
-#
-#		username = self.get_argument("username")
-#		password = self.get_argument("password")
-#
-#		print username, password
-#
-#		if username == "jamesduan" and password == "bios_suererdlxmn":
-#			print "login success and create session."
-#			session = Session()
-#			session.__setattr__('username', username)
-#			print "write to page"
-#			self.set_header("Access-Control-Allow-Origin", "*")
-#			self.write(session.session_id + session.username)
-#		else:
-#			print "login error, please try again.!"
-#			self.write("login error!")
+class LoginService(pyrestful.rest.RestHandler):
 
-#class MyApplication(Application):
-#
-#    def __init__(self):
-#
-#        handlers = [
-#            (r'/upload_html', UploadHtmlHandler),
-#            (r'/login', LoginHandler),
-#        ]
-#
-#        settings = dict()
-#
-#        Application.__init__(self, handlers, settings)
+	@post(_path='/login', _types=[str, str, str], _produces=mediatypes.APPLICATION_JSON)
+	def login_h(self, username, password, email):
+
+		user = dict(usr_name = username,
+					passwd = password,
+					mail = email)
+		return user
 
 def create_server():
-	print "start echo service..."
-	app = pyrestful.rest.RestService([EchoService])
-	app.listen(9999)
-	tornado.ioloop.IOLoop.instance().start()
+
+	logging.debug("parse command line...")
+	parse_command_line()
+	service_list = [
+		EchoService,
+		LoginService
+	]
+	logging.debug("create app by service list...")
+	app = pyrestful.rest.RestService(service_list)
+	logging.debug("listening in " + str(options.port))
+	app.listen(options.port)
+	logging.debug("instance start ...")
+	IOLoop.instance().start()
 
 if __name__ == "__main__":
+
 	try:
 		create_server()
+	except KeyboardInterrupt, e:
+		print "Canceled by user typed : Ctrl+C ", e
 	except Exception, e:
 		print e
+
